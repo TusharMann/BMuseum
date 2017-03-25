4453b8ef677ccdbf6b1802d9ac10d4929c789b53
Tushar Mann <tusharmann29@gmail.com>
2017-03-25 13:21:40 +0530

    database and nodejs files

---
+++ Museum/api4app1.js
@@ -0,0 +1,18 @@
+var express = require('express.);
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var url = require('url');
+
+mongoose.connect('mongodb://localhost/appdb');
+var app = express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+//Routes
+app.use('/api4app',require('./routes/api4app1'));
+
+//Start server 
+app.listen(1337,"0.0.0.0",function() {
+	console.log('Listening at port 1337');
+});
