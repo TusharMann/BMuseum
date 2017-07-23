f7b7f857d29497855bfbde5f8797410aba2cfe34
Aakash Singh <muserocks111@gmail.com>
2017-07-23 08:31:18 +0530

    Main Reference Library

---
+++ android-beacon-library-reference/app/src/main/java/org/altbeacon/beaconreference/MonitoringActivity.java
@@ -0,0 +1,155 @@
+package org.altbeacon.beaconreference;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+
+/**
+ * 
+ * @author dyoung
+ * @author Matt Tyler
+ */
+public class MonitoringActivity extends Activity  {
+	protected static final String TAG = "MonitoringActivity";
+	private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_monitoring);
+		verifyBluetooth();
+        logToDisplay("Application just launched");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons in the background.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                    @TargetApi(23)
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+
+                });
+                builder.show();
+            }
+        }
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+										   String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case PERMISSION_REQUEST_COARSE_LOCATION: {
+				if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					Log.d(TAG, "coarse location permission granted");
+				} else {
+					final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+					builder.setTitle("Functionality limited");
+					builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+					builder.setPositiveButton(android.R.string.ok, null);
+					builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+						@Override
+						public void onDismiss(DialogInterface dialog) {
+						}
+
+					});
+					builder.show();
+				}
+				return;
+			}
+		}
+	}
+
+	public void onRangingClicked(View view) {
+		Intent myIntent = new Intent(this, RangingActivity.class);
+		this.startActivity(myIntent);
+	}
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((BeaconReferenceApplication) this.getApplicationContext()).setMonitoringActivity(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        ((BeaconReferenceApplication) this.getApplicationContext()).setMonitoringActivity(null);
+    }
+
+	private void verifyBluetooth() {
+
+		try {
+			if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+				final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Bluetooth not enabled");			
+				builder.setMessage("Please enable bluetooth in settings and restart this application.");
+				builder.setPositiveButton(android.R.string.ok, null);
+				builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+					@Override
+					public void onDismiss(DialogInterface dialog) {
+						finish();
+			            System.exit(0);					
+					}					
+				});
+				builder.show();
+			}			
+		}
+		catch (RuntimeException e) {
+			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Bluetooth LE not available");			
+			builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+			builder.setPositiveButton(android.R.string.ok, null);
+			builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+				@Override
+				public void onDismiss(DialogInterface dialog) {
+					finish();
+		            System.exit(0);					
+				}
+				
+			});
+			builder.show();
+			
+		}
+		
+	}	
+
+    public void logToDisplay(final String line) {
+    	runOnUiThread(new Runnable() {
+    	    public void run() {
+    	    	EditText editText = (EditText)MonitoringActivity.this
+    					.findViewById(R.id.monitoringText);
+       	    	editText.append(line+"\n");            	    	    		
+    	    }
+    	});
+    }
+
+}
